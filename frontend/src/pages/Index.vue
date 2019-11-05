6ddf95ddaefe4621f8c170d8c1ec216f5bf0e712
Roylan Suarez <roylan@bandbcuba.com>
2019-11-05 11:32:04 -0500

    Se elimina codigo inicial no necesario en el proyecto

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,18 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    <q-input @input="hacerAlgo" v-model="text" label="Standard" />
-    {{ text }}
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'PageIndex',
-  
-  methods: {
-    hacerAlgo () {
-      console.log('haciendo algo')
-    }
-  }
 }
 </script>
